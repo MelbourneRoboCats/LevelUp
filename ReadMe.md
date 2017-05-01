7d53c49670338425d4b11fb14189101232f47495
David.Wyss <David.Wyss@gogs.net>
2017-05-01 13:08:26 +1000

    Added the level HTML files, as well as automated nav / footer resources.

--- ReadMe.md
+++ ReadMe.md
@@ -6,6 +6,7 @@ Four levels; assuming no knowledge of robots or the RoboCats team.
 Let potential new RoboCat team members understand what we do.
 Encourage more girls to join the team.
 Build the team spirit.
+By the end of the Levels, new RoboCats should be able to connect the controller, and drive the robot.
 
 ## Intended Audience
 Girls aged 13 to 18, interested in knowing more about becoming a RoboCat.
@@ -15,9 +16,9 @@ Audience has no pre-existing knowledge of the team or robotics.
 Fun.
 Team spirit building.
 
-##Platform
+## Platform
 Web hosted for first level. Subsequent levels are in person with a mentor and
 robot hardware.
 
-
-# 
+# Level 1
+Getting started <a href="htm/level1.htm">Level One</a>
---
+++ js/level.js
@@ -0,0 +1,96 @@
+
+
+var iNrOfLevelsToShow = 2;
+var iLevelNr = getLevelNr();
+
+function initPage() {
+	document.title = "Level " + iLevelNr + " RoboCats LevelUp";
+}
+
+function initNavigator() {
+	var sOut = "";
+	sOut += "<div id=\"navPrev\">";
+	if (iLevelNr == 1) {
+		sOut += "<span>&lt; Previous</span>";
+	} else {
+		sOut += "<a href=\"level" + (iLevelNr - 1) + ".htm\">&lt; Previous Level</a>";
+	}
+	sOut += "</div>";
+	sOut += "<div id=\"navNext\">";
+	if (iLevelNr == iNrOfLevelsToShow) {
+		sOut += "<span>Next &gt;</span>";
+	} else {
+		sOut += "<a href=\"level" + (iLevelNr + 1) + ".htm\">Next Level &gt;</a>";
+	}
+	sOut += "</div>";
+	sOut += "";
+	elAll("#navigator", sOut);
+}
+function getLevelNr() {
+	var sOut = document.location.href;
+	var asOut = sOut.split("htm/level").join(" ").split(".htm").join(" ").split(" ");
+	return parseInt(asOut[1]);
+}
+var wiki = {
+	format: function() {
+		var divWikiContent = el("wikiFormat");
+		var asWikiContent = divWikiContent.innerHTML.split("\n");
+		var sOut = "";
+		for (var iLine = 0; iLine < asWikiContent.length; iLine++) {
+			var sLine = asWikiContent[iLine];
+			var sStart = "";
+			var sEnd = "";
+			if (sLine.indexOf("## ") == 0) {
+				sLine = sLine.substring(3);
+				sStart = "<h3>";
+				sEnd = "</h3>";
+			} else if (sLine.indexOf("# ") == 0) {
+				sLine = sLine.substring(2);
+				sStart = "<h1>";
+				sEnd = "</h1>";
+			} else if (sLine.indexOf("* ") == 0) {
+				sLine = "&bull; " + sLine.substring(2);
+				//sStart = "<h1>";
+				//sEnd = "</h1>";
+			}
+			sOut += sStart + sLine + sEnd + "\n";
+		}
+		divWikiContent.innerHTML = sOut;
+	}
+}
+function initFooter() {
+	var sOut = "<p class=\"site-credits\">Official Sponsors</p>";
+	sOut += "<div id=\"sponsorLogosWrapper\">";
+	sOut += "<img src=\"../img/logoSwinburnMedium.png\" />";
+	sOut += "<img src=\"../img/logoBaeSystemsMedium.png\" />";
+	sOut += "<img src=\"../img/logoFordMedium.png\" />";
+	sOut += "<img src=\"../img/logoInvetechMedium.png\" />";
+	sOut += "<img src=\"../img/logoBoengMedium.png\" />";
+	sOut += "<img src=\"../img/logoSalesForceMedium.png\" />";
+	sOut += "<img src=\"../img/logoRockwellAutomationMedium.png\" />";
+	sOut += "</div>";
+	var oFooter = el("#footer div.contentSectionWrapper", sOut);
+}
+function elAll(sXpath, sContent) {
+	var aoOut = document.querySelectorAll(sXpath);
+	for (var iEl = 0; iEl < aoOut.length; iEl++) {
+		if (typeof sContent != "undefined") {
+			aoOut[iEl].innerHTML = sContent;
+		}
+	}
+	return aoOut;
+}
+function el(sXpath, sContent) {
+	var oOut = document.querySelector(sXpath);
+	if (typeof sContent != "undefined") {
+		oOut.innerHTML = sContent;
+	}
+	return oOut;
+}
+function doOnLoad() {
+	// Set up navigator.
+	initPage();
+	initNavigator();
+	initFooter();
+	wiki.format();
+}
